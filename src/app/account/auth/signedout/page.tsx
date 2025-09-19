5b03fca6033707ed113bc84696552512c400cf93
Antony Holmes <antony@antonyholmes.dev>
2025-09-19 13:50:24 -0400

    Changed: Add suspense to signout pages

--- src/app/account/auth/signedout/page.tsx
+++ src/app/account/auth/signedout/page.tsx
@@ -1,8 +1,13 @@
 import { SignedOutPage } from '@/components/pages/account/auth/oauth2/signedout-page'
 import { makeMetaData } from '@lib/metadata'
+import { Suspense } from 'react'
 
 export const metadata = makeMetaData('Signed Out')
 
 export default function Page() {
-  return <SignedOutPage />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignedOutPage />
+    </Suspense>
+  )
 }
--- src/app/account/auth/signout/page.tsx
+++ src/app/account/auth/signout/page.tsx
@@ -1,8 +1,13 @@
 import { SignOutPage } from '@/components/pages/account/auth/oauth2/signout-page'
 import { makeMetaData } from '@lib/metadata'
+import { Suspense } from 'react'
 
 export const metadata = makeMetaData('Sign Out')
 
 export default function Page() {
-  return <SignOutPage />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignOutPage />
+    </Suspense>
+  )
 }
