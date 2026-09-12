b4047b8b4045d78f9288b9877109bc3f0e79cbe5
Antony Holmes <antony@antonyholmes.dev>
2026-09-12 01:45:22 -0400

    Changed: Bug fixes and updates.

--- src/lib/gsea/geneset.ts
+++ src/lib/gsea/geneset.ts
@@ -2,15 +2,39 @@ import type { IDBEntity } from '@/interfaces/db-entity'
 import { randomHexColor } from '../color/color'
 import { makeUuid } from '../id'
 
+/**
+ * Represents a gene and its associated score
+ * which can be useful in Viper output where the
+ * gene has an associated score to indicate
+ * strength or significance of its association.
+ * For basic GSEA, the score may not be necessary and can be ignored.
+ */
 export interface IScoreGene {
   name: string
   score: number
 }
 
 export interface IGeneSet extends IDBEntity {
+  /**
+   * The name of the gene set.
+   */
+  name: string
+
+  /**
+   * The genes contained in the gene set. Each gene can be represented
+   * either as an IScoreGene object or as a string (gene name).
+   * If a gene is represented as a string, it is assumed to have
+   * a default score of 1 so that scaling by gene score can be ignored
+   * as multiplying by 1 has no effect. If a gene is represented as an
+   * IScoreGene object, its score will be used for scaling.
+   */
   genes: (IScoreGene | string)[]
+
+  /**
+   * Optional color associated with the gene set.
+   * This can be used for visualization purposes.
+   */
   color?: string
-  //type: 'geneset'
 }
 
 export interface ICollection extends IDBEntity {
@@ -50,10 +74,25 @@ export function makeNewGeneset(name: string = 'Gene Set 1'): IGeneSet {
   }
 }
 
+/**
+ * Returns the names of the genes in the gene set. If geneset genes are
+ * represented as strings, their names are used directly.
+ *
+ * @param geneset The gene set from which to extract gene names.
+ * @returns An array of gene names contained in the gene set.
+ */
 export function geneSetNames(geneset: IGeneSet): string[] {
   return geneset.genes.map((g) => (typeof g === 'string' ? g : g.name))
 }
 
+/**
+ * Returns the scores of the genes in the gene set. If geneset genes are
+ * represented as strings, they are assigned a default score of 1,
+ * while numeric scores are used as-is.
+ *
+ * @param geneset The gene set from which to extract gene scores.
+ * @returns An array of gene scores contained in the gene set. If a gene is represented as a string, it is assigned a default score of 1.
+ */
 export function geneSetScores(geneset: IGeneSet): number[] {
   return geneset.genes.map((g) => (typeof g === 'string' ? 1 : g.score))
 }
